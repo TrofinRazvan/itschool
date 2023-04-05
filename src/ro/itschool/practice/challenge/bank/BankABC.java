3c3ec865e76cd8132ae136c4122c4b1c2b691240
Razvan <razvantrofin743@yahoo.com>
2023-04-05 22:15:41 +0300

    session

--- src/ro/itschool/practice/challenge/bank/BankABC.java
+++ src/ro/itschool/practice/challenge/bank/BankABC.java
@@ -4,6 +4,7 @@ public class BankABC {
     public static void main(String[] args) {
         BankA bankA = new BankA();
         bankA.getBalance();
+
         BankB bankB = new BankB();
         bankB.getBalance();
         BankC bankC = new BankC();
