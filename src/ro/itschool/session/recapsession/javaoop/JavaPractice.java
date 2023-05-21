7ded8c4e37178becd9ba458a06d30ae9c3a9a897
Razvan <razvantrofin743@yahoo.com>
2023-05-21 14:11:33 +0300

    update

---
+++ src/ro/itschool/session/recapsession/javaoop/JavaPractice.java
@@ -0,0 +1,15 @@
+package ro.itschool.session.recapsession.javaoop;
+
+public class JavaPractice {
+
+    public JavaPractice() {}
+
+    public static void main(String[] args) {
+        JavaPractice javaPractice = new JavaPractice();
+        System.out.println("hello");
+    }
+
+    public int getSumOfNumbers(int a, int b) {
+        return a + b;
+    }
+}
--- src/ro/itschool/whatIdid/codewars/InvertValues.java
+++ src/ro/itschool/whatIdid/codewars/InvertValues.java
@@ -1,14 +1,14 @@
-package ro.itschool.whatIdid.codewars;
-
-public class InvertValues {
-
-    public static void main(String[] args) {
-
-        int[] array = {1, 2, -3, -4, 5};
-        for (int index = 0; index < array.length; index++) {
-                array[index] *= -1;
-            }
-            return array;
-        }
-
-    }
+//package ro.itschool.whatIdid.codewars;
+//
+//public class InvertValues {
+//
+//    public static void main(String[] args) {
+//
+//        int[] array = {1, 2, -3, -4, 5};
+//        for (int index = 0; index < array.length; index++) {
+//                array[index] *= -1;
+//            }
+//            return array;
+//        }
+//
+//    }
