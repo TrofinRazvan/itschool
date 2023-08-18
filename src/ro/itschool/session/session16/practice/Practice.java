c1d6ed15a714a1a628989ece3a4e6ced3c30e749
Razvan <razvantrofin743@yahoo.com>
2023-08-18 15:41:20 +0300

    S16 - practice

---
+++ src/ro/itschool/session/session16/practice/Practice.java
@@ -0,0 +1,11 @@
+package ro.itschool.session.session16.practice;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+
+        Practice practice = new Practice();
+        System.out.println(practice.hashCode());
+    }
+}
